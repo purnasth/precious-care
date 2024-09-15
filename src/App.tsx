94ba2033c0f80b74d3d16e7034a3020e3571ede2
Purna Shrestha <crush9918@gmail.com>
2024-09-15 11:58:08 +0545

    Update display component and add more declares

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from "react";
+import Display from "./components/Display";
 
 const App: React.FC = () => {
   return (
-    <main className="container mx-auto p-4 flex items-center justify-center flex-col gap-12 text-center h-screen">
-      <img src="https://scontent.fktm10-1.fna.fbcdn.net/v/t39.30808-6/300059041_2003939946661609_302555476645528495_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=d5GLgpN07BcQ7kNvgHEPfGw&_nc_ht=scontent.fktm10-1.fna&oh=00_AYDcJkiDI8UoiqdWqDmDE2VIHp28vQDHm48I63OdbXybYA&oe=66EBC4D0" alt="Precious Care Collection" className="size-36 rounded-full object-contain" />
-      <h1 className="text-5xl font-luxury">Precious Care Collection</h1>
-
-      <p className="max-w-3xl">
-        Jewellery Store in Jyatha Thamel since 2016. Remember for all kinds of
-        silver and metal jewellery, semi precious stones at reasonable price and
-        quality. Get what u think it is. We give full information on what u are
-        buying.
-      </p>
-    </main>
+    <>
+      <Display />
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}", 
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: "#D4AF37", 
-        secondary: "#C0C0C0", 
+        primary: "#D4AF37",
+        secondary: "#C0C0C0",
+        dark: "#000111",
       },
       fontFamily: {
-        body: ["Titillium Web", "sans-serif"], 
-        luxury: ['Glamora', 'sans-serif'],
+        body: ["Titillium Web", "sans-serif"],
+        luxury: ["Glamora", "sans-serif"],
+      },
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+      "3xl": "1920px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        sm: "100%",
+        md: "100%",
+        lg: "1024px",
+        xl: "1280px",
       },
     },
   },
